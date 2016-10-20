aee5b7d426f8158dbc4e84943c8e3ad50ca35898
gregoriusandito <gregorius.andito@gmail.com>
2016-10-20 14:01:32 +0700

    Create EditStudentActivity

---
+++ app/src/main/java/com/insiteprojectid/practiceadapter/EditStudentActivity.java
@@ -0,0 +1,105 @@
+package com.insiteprojectid.practiceadapter;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.insiteprojectid.practiceadapter.user.StaticStudent;
+import com.insiteprojectid.practiceadapter.user.Student;
+
+import java.util.zip.Inflater;
+
+public class EditStudentActivity extends AppCompatActivity {
+
+    private EditText idText;
+    private EditText nameText;
+    private EditText noregText;
+    private EditText phoneText;
+    private EditText mailText;
+    private Student student;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_student);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        idText = (EditText) findViewById(R.id.edit_id);
+        nameText = (EditText) findViewById(R.id.edit_nama);
+        noregText = (EditText) findViewById(R.id.edit_nim);
+        phoneText = (EditText) findViewById(R.id.edit_phone);
+        mailText = (EditText) findViewById(R.id.edit_email);
+
+        //get parcel
+        Student object = getIntent().getParcelableExtra("StudentList");
+        //implements parcel
+        idText.setText("" + object.getId());
+        nameText.setText(object.getName());
+        noregText.setText(object.getNoreg());
+        phoneText.setText(object.getPhone());
+        mailText.setText(object.getMail());
+
+        FloatingActionButton fabDone = (FloatingActionButton) findViewById(R.id.fabDone);
+        FloatingActionButton fabCancel = (FloatingActionButton) findViewById(R.id.fabCancel);
+
+        fabDone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //close activity
+                finish();
+            }
+        });
+        fabCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //release the krakens (the krakens will crawl ur data and put it to the bucket of darkness (ArrayList), lol)
+                saveEditedData();
+            }
+        });
+
+    }
+
+    protected void saveEditedData(){
+        int id = Integer.parseInt(idText.getText().toString());
+        String noreg = noregText.getText().toString();
+        String name = nameText.getText().toString();
+        String phone = phoneText.getText().toString();
+        String mail = mailText.getText().toString();
+        student = new Student(id, noreg, name, mail, phone);
+        StaticStudent staticStudent = StaticStudent.getInstance();
+        staticStudent.set(id, student);
+        Toast success = Toast.makeText(getApplicationContext(), "Edit Success", Toast.LENGTH_SHORT);
+        success.show();
+        finish();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_edit,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.deleteItem:
+                //get current id
+                int id = student.getId();
+                StaticStudent data = StaticStudent.getInstance();
+                data.remove(id);
+                finish();
+                return true;
+        }
+        return false;
+    }
+}
